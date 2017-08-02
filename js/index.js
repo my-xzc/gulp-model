787b33aa5a06465e48e382488e8fe7c00770b115
XIAZHENGCHUN\admin <1056073482@qq.com>
2017-08-03 00:18:17 +0800

    222

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () { 
         // console.log( $('#scroller')[0].scrollHeight);
                  
-        var OneHight = $("#scrollerOne").height();  
-        var ScrollOneHight = $("#scrollerOne")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
-        var TwoHight = $("#scrollerTwo").height();  
-        var ScrollTwoHight = $("#scrollerTwo")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
-        var ThreeHight = $("#scrollerThree").height();  
-        var ScrollThreeHight = $("#scrollerThree")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
-        console.log(OneHight,ScrollOneHight,TwoHight,ScrollTwoHight,ThreeHight,ScrollThreeHight)
+        var OneHight = $("#wrapperOne").height();  
+        var ScrollOneHight = $("#wrapperOne")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
+        var TwoHight = $("#wrapperTwo").height();  
+        var ScrollTwoHight = $("#wrapperTwo")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
+        var ThreeHight = $("#wrapperThree").height();  
+        var ScrollThreeHight = $("#wrapperThree")[0].scrollHeight; //滚动距离总长(注意不是滚动条的长度)
+        console.log(OneHight,ScrollOneHight,TwoHight,ScrollTwoHight,ThreeHight,ScrollThreeHight);
         next();
         function next(){
                 let windowHeight = $(window).height(),
@@ -26,10 +26,10 @@ $(document).ready(function () {
         　　　　X = moveEndX - startX,
                     Y = moveEndY - startY;
             let index = $(this).parent().index();        
-            console.log($(this).index());
                 if ( Math.abs(Y) > Math.abs(X) && Y > 0) {
-                        if ((Y>(ScrollOneHight-OneHight) &&Y>(ScrollTwoHight-TwoHight) &&Y>(ScrollThreeHight-ThreeHight))&& index>0){ 
-                                console.log(Y, '到上一页', $(this).parent().prev().css('width'), $(this));
+                        if (((Y*4) >(ScrollOneHight-OneHight) &&(Y*4) >(ScrollTwoHight-TwoHight) && (Y*4)  >(ScrollThreeHight-ThreeHight))&& index>0){ 
+                        //  if (Y*3 >= $(this).offset().top&& index>0){ 
+                                console.info(Y, '到上一页',$(this).offset().top);
                                 let widthP = $(this).parent().prev().css('width');
                                         if(index == len-1){
                                                 $('.page').eq(0).fadeIn(); 
@@ -37,20 +37,22 @@ $(document).ready(function () {
                                         
                                         $(this).parent().addClass('slideOutDown').removeClass('slideInUp').prev()
                                         .animate({
-                                        "width":( widthP*1.3)+"px",
-                                        // left:'3%'
+                                        "width":"94%",
+                                        "left":'3%'
                                 })
                         }    
                 }
                   
         　　　　else if ( Math.abs(Y) > Math.abs(X) && Y < 0 ) {
-                        if ((Y<(OneHight-ScrollOneHight) && Y<(TwoHight-ScrollTwoHight) && Y<(ThreeHight-ScrollThreeHight)) && index < len-1){ 
-                                console.log(Y, '到下一页',$(this).parent().next().css('width'),$(this));
+                        if (((Y*4) <(OneHight-ScrollOneHight) && (Y*4) <(TwoHight-ScrollTwoHight) && (Y*4) <(ThreeHight-ScrollThreeHight)) && index < len-1){ 
+                                // if (Y*3 <= $(this).offset().top && index < len-1){ 
+                                console.info(Y,'到下一页',$(this).offset().top);
+
                                 let widthN = $(this).parent().next().css('width');
                                 $(this).parent().next().removeClass('slideOutDown');
                                 $(this).parent().removeClass('slideOutDown slideInUp').animate({
-                                        width: (widthN*.6)+"px",
-                                        // left: width-5%
+                                        "width": "90%",
+                                        "left": "5%"
                                 }).next().show().addClass('slideInUp');
                                 if(index==1){
                                 $('.page').eq(0).fadeOut();
